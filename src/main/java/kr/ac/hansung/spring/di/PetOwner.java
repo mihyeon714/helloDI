07da3304ba707ce03aed1975c35c93dca86469b8
mihyeon714 <mihyeon714@gmail.com>
2016-10-11 19:03:31 +0900

    helloDI_useAnnotation

--- src/main/java/kr/ac/hansung/spring/di/PetOwner.java
+++ src/main/java/kr/ac/hansung/spring/di/PetOwner.java
@@ -1,11 +1,24 @@
 package kr.ac.hansung.spring.di;
 
-public class PetOwner {
-	private AnimalType animal;
+import javax.annotation.Resource;
 
-	public PetOwner(AnimalType animal) {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class PetOwner {
+	
+	//@Autowired //wiring by type
+	//@Qualifier(value = "qt_cat")
+	
+	@Resource(name="dog") //wiring by name
+	
+	private AnimalType animal;
+	
+
+	/*public PetOwner(AnimalType animal) {
 		this.animal = animal;
 	}
+	*/
 
 	public void play() {
 		animal.sound();
